797ae4f858f689d522c6a1dd570077469586f728
Felix Fung <felix.the.cheshire.cat@gmail.com>
2014-05-23 04:37:24 -0400

    An enhancement algorithm

--- pixelo_solver.py
+++ pixelo_solver.py
@@ -5,12 +5,10 @@ from PIL import Image
 from array import array
 
 ANSWER = (53, 131)
+DIGIT_WIDTH = 18
+DIGIT_HEIGHT = 16
 
 def get_pixel(obj, c, r):
-  #pixel_a = img_a[pos_a[0] + c, pos_a[1] + r][:3]
-  #pixel_b = img_b.getpixel((pos_b[0] + c, pos_b[1] + r))
-  #start = obj["height"] * 4 * r + c * 4
-  #return (obj["arr"])[start:start+4]
   return obj[c, r]
 
 def norm2(pixel_a, pixel_b):
@@ -32,7 +30,6 @@ def img_distance(img_a, pos_a, img_b, pos_b, width, height):
         nm = norm2(pixel_a, pixel_b)
 
       pixels += 1
-      #print nm
       err += nm
 
   return err / pixels
@@ -56,16 +53,32 @@ def img_indexof(haystack, hsize, needle, nsize):
   h_width, h_height = hsize
   n_width, n_height = nsize
 
-  #img_subequal(h, ANSWER, n, (0, 0), n_width, n_height)
-
-  for r in range(0, h_height - n_height + 1):
-    for c in range(0, h_width - n_width + 1):
+  for c in range(0, h_width - n_width + 1):
+    for r in range(0, h_height - n_height + 1):
       if img_subequal(haystack, (c, r), needle, (0, 0), n_width, n_height):
         return (c, r)
   return (-1, -1)
 
 def digit_crop(screen, left, top):
   return screen.crop((left, top, left + DIGIT_WIDTH, top + DIGIT_HEIGHT))
+
+def luminance(pixel):
+  return 0.2126 * (pixel[0] / 255.0) + 0.7152 * (pixel[1] / 255.0) + 0.0722 * (pixel[2] / 255.0)
+
+def enhance_digit(crop):
+  for r in range(0, DIGIT_HEIGHT):
+    mn = crop[0, r]
+    for c in range(1, DIGIT_WIDTH):
+      if luminance(crop[c, r]) < luminance(mn):
+        mn = crop[c, r]
+
+    for c in range(0, DIGIT_WIDTH):
+      if abs(luminance(crop[c, r]) - luminance(mn)) < 0.1:
+        crop[c, r] = (0, 0, 0, 255)
+      else:
+        crop[c, r] = (255, 255, 255, 255)
+
+    #print mn
 
 def load_templates():
   templates = [ None ]
@@ -85,48 +98,40 @@ def template_analysis(haystack, hsize, templates):
     template = templates[x]
     tsize = template["size"]
     left = (h_width - tsize[0]) / 2
-    #print "TEMPLATE ANALYSIS START:", x
     print x, img_distance(haystack, (left, 0), template["pix"], (0, 0), tsize[0], tsize[1])
 
 if __name__ == "__main__":
-  DIGIT_WIDTH = 18
-  DIGIT_HEIGHT = 16
-
   OFFSET_HEIGHT = 20
 
   GRID_SIDE = 30
 
-  LEFT_FROM_ANCHOR = 306
+  LEFT_FROM_ANCHOR = 11
   #TOP_FROM_ANCHOR = 73
-  TOP_FROM_ANCHOR = 90
+  TOP_FROM_ANCHOR = -20
 
   # TODO: Convert Image object into a fast pixelaccess object with size and width
   screen = Image.open("example_screen2.png")
   screen = screen.convert("RGBA")
   screen_pix = screen.load()
 
-  anchor = Image.open("pixelo_anchor.png")
+  anchor = Image.open("pixelo_anchor_3.png")
   anchor_pix = anchor.load()
 
   templates = load_templates()
 
   apos = img_indexof(screen_pix, screen.size, anchor_pix, anchor.size)
   bpos = (apos[0] + LEFT_FROM_ANCHOR, apos[1] + TOP_FROM_ANCHOR)
-  cpos = (apos[0] + LEFT_FROM_ANCHOR + GRID_SIDE, apos[1] + TOP_FROM_ANCHOR)
+  cpos = (apos[0] + LEFT_FROM_ANCHOR + 2 * GRID_SIDE - 1, apos[1] + TOP_FROM_ANCHOR)
 
-  # This is a 3
-  crop = digit_crop(screen, bpos[0], bpos[1])
+  ## This is a 9
+  crop = digit_crop(screen, cpos[0], cpos[1])
   crop_pix = crop.load()
   crop.show()
+  enhance_digit(crop_pix)
   template_analysis(crop_pix, crop.size, templates)
-
-  # This is a 5
-  #porc = digit_crop(screen, cpos[0], cpos[1])
-  #porc_pix = porc.load()
-  #template_analysis(porc_pix, porc.size, templates)
-
-  print "=============================================="
 
   hc = Image.open("mc.png")
   hc_pix = hc.load()
+  enhance_digit(hc_pix)
+
   template_analysis(hc_pix, hc.size, templates)
